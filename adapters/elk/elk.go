4477d24436a1b2b218ff79368535fa4f65c4172a
Jevon <github@jevonwild.com>
2015-03-23 15:45:14 -0700

    Testing ELK Adapter

--- adapters/elk/elk.go
+++ adapters/elk/elk.go
@@ -6,9 +6,10 @@ import (
 	"io"
 	// "log/syslog"
 	"net"
-	"os"
+	// "os"
 	// "strings"
 	// "time"
+	"encoding/json"
 
 	"github.com/delectable/logspout/router"
 )
@@ -17,13 +18,13 @@ func init() {
 	router.AdapterFactories.Register(NewElkAdapter, "elk")
 }
 
-func getopt(name, dfault string) string {
-	value := os.Getenv(name)
-	if value == "" {
-		value = dfault
-	}
-	return value
-}
+// func getopt(name, dfault string) string {
+// 	value := os.Getenv(name)
+// 	if value == "" {
+// 		value = dfault
+// 	}
+// 	return value
+// }
 
 func NewElkAdapter(route *router.Route) (router.LogAdapter, error) {
 	transport, found := router.AdapterTransports.Lookup(route.AdapterTransport("udp"))
@@ -48,28 +49,31 @@ type ElkAdapter struct {
 
 func (adapter *ElkAdapter) Stream(logstream chan *router.Message) {
 	for message := range logstream {
-		output_string := fmt.Sprintf("{entry: {time: %d, message: %s}}", message.Time.Unix, message.Data)
-		io.WriteString(adapter.conn, output_string)
-		// err := a.tmpl.Execute(a.conn, &ElkMessage{message, a})
-		// if err != nil {
-		// 	log.Println("syslog:", err)
-		// 	a.route.Close()
-		// 	return
-		// }
+		elkMessage = NewElkMessage(message)
+		io.WriteString(adapter.conn, elkMessage.ToString())
 	}
 }
 
-// func (m *ElkMessage) Priority() syslog.Priority {
-// 	switch m.Message.Source {
-// 	case "stdout":
-// 		return syslog.LOG_USER | syslog.LOG_INFO
-// 	case "stderr":
-// 		return syslog.LOG_USER | syslog.LOG_ERR
-// 	default:
-// 		return syslog.LOG_DAEMON | syslog.LOG_INFO
-// 	}
-// }
+type ElkMessage struct {
+	routerMessage *router.Message
+	object        struct {
+		Time    int    `json: "time"`
+		Message string `json: "message"`
+	}
+}
 
-// func (m *ElkMessage) ContainerName() string {
-// 	return m.Message.Container.Name[1:]
-// }
+func NewElkMessage(routerMessage *router.Message) ElkMessage {
+	elkMessage := &ElkMessage{
+		routerMessage: routerMessage,
+		object: {
+			Time:    routerMessage.Time.Unix,
+			Message: routerMessage.Data,
+		},
+	}
+	return elkMessage
+}
+
+func (elkMessage *ElkMessage) ToString() {
+	return_string, _ = json.Marshal(elkMessage.object)
+	return string(return_string)
+}
